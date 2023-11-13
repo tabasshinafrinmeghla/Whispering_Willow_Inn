e37811c4dc1829176eff61eb42876afe671a2b28
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2023-11-13 21:12:16 +0600

    navbar done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
+import img from '../../img/WWI.jpg'
 
 const Header = () => {
   return (
-    <div>
-      <Link to="/about">About</Link>
-      <Link to="/room">Room</Link>
-      <Link to="/booking">Booking</Link>
-      <Link to="/contact">Contact</Link>
+    <div className='header'>
+      <div >
+        <img className='w-24 rounded-full ms-10' src={img} alt="" />
+      </div>
+
+      <div className="navbar-center  m-10">
+
+        <Link className='m-5  hover:bg-sky-800 px-3 hover:text-gray-50' to="/">Home</Link>
+        <Link className='m-5  hover:bg-sky-800 px-3 hover:text-gray-50' to="/about">About</Link>
+        <Link className='m-5  hover:bg-sky-800 px-3 hover:text-gray-50' to="/room">Room</Link>
+        <Link className='m-5  hover:bg-sky-800 px-3 hover:text-gray-50' to="/booking">Booking</Link>
+        <Link className='m-5  hover:bg-sky-800 px-3 hover:text-gray-50' to="/contact">Contact</Link>
+
+      </div>
+
+
+
+
     </div>
+
+
   );
 };
 
--- src/Components/Layout/Main.jsx
+++ src/Components/Layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 const Main = () => {
   return (
     <nav>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </nav>
   );
 };
