7eb2353fafe77547d4879c5bf3fa82b66e19d2b8
TabasshinAfrinMeghla <tabasshinafrinmeghla@gmail.com>
2023-11-14 23:59:03 +0600

    added

--- src/Components/pages/Home/Home.jsx
+++ src/Components/pages/Home/Home.jsx
@@ -13,19 +13,111 @@ const Home = () => {
     <div>
       <BackgroundImage></BackgroundImage>
 
+      <div className='bg-slate-800 p-7 my-8  shadow-indigo-500/40'>
+        <div className='flex'>
+          <div className='w-1/2 '>
+            <h2 className='capitalize text-justify p-2 font-serif text-xl'>High-Quality Accommodations:</h2>
+            <p className='text-justify p-4 indent-4'>Luxurious hotels typically offer spacious and well-appointed rooms or suites with upscale furnishings, comfortable bedding, and high-end amenities. Guests can expect top-notch interior design and attention to detail.</p>
+          </div>
+          <div className='w-1/2 m-8'>
+            <div className="card  bg-base-100 shadow-xl image-full flex-1  shadow-indigo-500/40 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+              <figure>
+                <img src={img3} alt="" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Room!</h2>
+                <p>If a dog chews Room whose Room does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary "><Link to="/vip room">BookIng</Link></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='flex'>
+          <div className='w-1/2 m-8'>
+            <div className="card  bg-base-100 shadow-xl image-full flex-1  shadow-indigo-500/40 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+              <figure>
+                <img src={img2} alt="" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Room!</h2>
+                <p>If a dog chews Room whose Room does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary "><Link to="/vip room">BookIng</Link></button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='w-1/2 '>
+            <h2 className='capitalize text-justify p-2 font-serif text-xl'>Exceptional Service:</h2>
+            <p className='text-justify p-4 indent-4'>Service is a key element of luxury hotels. Staff members are often highly trained, attentive, and aim to anticipate and fulfill guests' needs promptly. Personalized services, such as concierge assistance, are common.</p>
+          </div>
+
+        </div>
+
+
+        <div className='flex'>
+          <div className='w-1/2 '>
+            <h2 className='capitalize text-justify p-2 font-serif text-xl'>Fine Dining:</h2>
+            <p className='text-justify p-4 indent-4'>Luxury hotels often feature one or more high-quality restaurants with renowned chefs, offering a diverse range of culinary experiences. Some hotels may also provide 24-hour room service with gourmet menu options.</p>
+          </div>
+          <div className='w-1/2 m-8'>
+            <div className="card  bg-base-100 shadow-xl image-full flex-1  shadow-indigo-500/40 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+              <figure>
+                <img src={img1} alt="" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Room!</h2>
+                <p>If a dog chews Room whose Room does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary "><Link to="/vip room">BookIng</Link></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='flex'>
+          <div className='w-1/2 m-8'>
+            <div className="card  bg-base-100 shadow-xl image-full flex-1  shadow-indigo-500/40 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300">
+              <figure>
+                <img className='w-80' src={img4} alt="" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">Room!</h2>
+                <p>If a dog chews Room whose Room does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary "><Link to="/vip room">BookIng</Link></button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='w-1/2 '>
+            <h2 className='capitalize text-justify p-2 font-serif text-xl'>Spa and Wellness Facilities:</h2>
+            <p className='text-justify p-4 indent-4'>Many luxurious hotels have on-site spas, fitness centers, and wellness facilities. These may include massage services, saunas, steam rooms, and state-of-the-art fitness equipment.</p>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
 
 
       <div class="grid grid-cols-6 gap-4">
         <div class="col-start-2 col-span-4 ...">
           <div className="card  glass">
             <figure>
-              <img className='pt-2' src={img2} alt="" />
+              <img className='pt-2 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300' src={img2} alt="" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">Life hack</h2>
+              <h2 className="card-title">Choose your Room</h2>
               <p>How to park your car at your garage?</p>
               <div className="card-actions justify-end">
-                <button className="btn btn-primary">Learn now!</button>
+                <button className="btn btn-primary"><Link to="/vip room">BookIng</Link></button>
               </div>
             </div>
           </div>
